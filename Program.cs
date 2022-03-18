bae7ed2a6ed5c63bc750ff1189cea6788a2a8404
Jazmin Guadalupe Hernandez Rodriguez <19030545@uadeo.mx>
2022-03-17 17:53:16 -0700

    Segunda version

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace ActividadGitConsola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float n1 = 0; float n2 = 0;
+
+            Console.WriteLine("Calculadora \r");
+            Console.WriteLine("------------------------\n");
+
+            Console.WriteLine("Ingrese un Numero:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese otro Numero: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
