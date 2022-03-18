719a03cf8678f85a4ce24ef852cb5972d260d979
JenniferFelix <89956606+JenniferFelix@users.noreply.github.com>
2022-03-18 00:01:08 -0700

    Septima Version
    Se agrego colores a la calculadora

--- Program.cs
+++ Program.cs
@@ -8,15 +8,18 @@ namespace ActividadGitConsola
         {
             float n1 = 0; float n2 = 0;
 
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Calculadora \r");
             Console.WriteLine("------------------------\n");
 
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("Ingrese un Numero:");
             n1 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Ingrese otro Numero: ");
             n2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Escoja una de las opciones de la lista:");
             Console.WriteLine("\ta - Añadir");
             Console.WriteLine("\ts - Subtraer");
@@ -24,6 +27,8 @@ namespace ActividadGitConsola
             Console.WriteLine("\td - Dividir");
             Console.Write("Cual es tu opcion? ");
 
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
             switch (Console.ReadLine())
             {
                 case "a":
